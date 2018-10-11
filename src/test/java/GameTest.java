456354693697bb1b1d5365578be71a7817d0cf57
Callum Anderson <callum_anderson@hotmail.com>
2018-10-11 14:59:27 +0100

    Game class added

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
 public class GameTest {
+    Player player1;
+    Player player2;
+    Deck deck;
+    Game game;
+
+    @Before
+    public void before(){
+        player1 = new Player();
+        player2 = new Player();
+        deck = new Deck();
+        game = new Game(player1, player2, deck);
+    }
+
+
 }
